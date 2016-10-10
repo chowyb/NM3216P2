954d5e33208b9e71f29970d3e741afeee31575a1
Yuan Bin <chowwars@hotmail.com>
2016-10-10 21:31:29 +0800

    pre-playtesting changes

--- Assets/scripts/GPS/TrapGPS.cs
+++ Assets/scripts/GPS/TrapGPS.cs
@@ -5,6 +5,12 @@ public class TrapGPS : MonoBehaviour {
 
 	private SharedValues sharedValues = SharedValues.GetInstance();
 
+	void Update() {
+		if (GetComponent<Renderer>() != null) {
+			GetComponent<Renderer>().enabled = !(SharedValues.GetInstance().areWallsShown);
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.CompareTag("Player")) {
 			sharedValues.isStunned = true;
