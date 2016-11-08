a31a788b47243519b8f73368d92c0c3740e623a4
Yuan Bin <chowwars@hotmail.com>
2016-11-09 00:24:15 +0800

    updated ReleaseA

---
+++ Assets/scripts/OPScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OPScript : MonoBehaviour {
+
+	private int frames = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			SceneManager.LoadScene("main");
+		}
+		frames++;
+		if (frames % 120 < 40) {
+			GetComponent<Image>().enabled = false;
+		}
+		else {
+			GetComponent<Image>().enabled = true;
+		}
+	}
+}
